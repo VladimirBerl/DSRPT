241b12473c5d1a7abec1eec680390167dbd3a1be
VladimirBerl <vova2002.13lol@gmail.com>
2025-01-02 17:57:03 +0300

    fix

--- components/shared/statistics-section.tsx
+++ components/shared/statistics-section.tsx
@@ -13,12 +13,14 @@ export default function StatisticsSection() {
   const sectionRef = React.useRef<HTMLDivElement | null>(null);
   const humberRef = React.useRef<HTMLHeadingElement | null>(null);
 
-  const [currentIndex, setCurrentIndex] = React.useState(0); // Индекс текущего массива
+  const [currentIndex, setCurrentIndex] = React.useState(0);
+
+  const imageArray = new Array(8).fill("/polygon.svg");
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
   const numbers = [
-    { number: 25, label: "Реализованных проектов" },
     { number: 7, label: "Отраслей: большой опыт работы" },
+    { number: 25, label: "Реализованных проектов" },
     { number: 300, label: "Коэффициент возврата инвестиций" },
   ];
 
@@ -34,16 +36,14 @@ export default function StatisticsSection() {
   };
 
   React.useEffect(() => {
-    // Запускаем анимацию при появлении блока в поле зрения
     if (humberRef.current) {
       gsap.to(humberRef.current, {
         scrollTrigger: {
           trigger: humberRef.current,
-          start: "top 80%", // Анимация запускается, когда верх блока окажется на 80% от экрана
-          end: "bottom top", // Анимация завершается, когда блок покидает экран
-          once: true, // Анимация запускается только один раз
+          start: "top 60%",
+          end: "bottom top",
+          once: true,
           onEnter: () => {
-            // Когда блок появляется в поле зрения
             runCountUpAnimation(numbers[currentIndex].number);
           },
         },
@@ -52,42 +52,69 @@ export default function StatisticsSection() {
   }, [currentIndex, numbers]);
 
   React.useEffect(() => {
-    // GSAP ScrollTrigger для фиксации блока при прокрутке
     if (sectionRef.current) {
       gsap.to(sectionRef.current, {
         scrollTrigger: {
           trigger: sectionRef.current,
-          start: "center center", // Начало анимации, когда верх блока достигнет центра экрана
-          end: "bottom top",   // Когда блок полностью уйдет
-          pin: true,           // Фиксируем блок на экране
-          scrub: true,         // Сделать анимацию плавной с прокруткой
-          // markers: true,       // Показывать маркеры для отладки
+          start: "center center",
+          end: "bottom top",
+          pin: true,
+          scrub: true,
           onUpdate: (self) => {
-            // self.progress возвращает значение от 0 до 1
-            const progress = self.progress; // Получаем прогресс анимации (от 0 до 1)
-            const index = Math.floor(progress * numbers.length); // Вычисляем индекс в зависимости от прогресса
-            setCurrentIndex(index % numbers.length); // Обновляем индекс с учетом модуля
+            const progress = self.progress;
+            const index = Math.floor(progress * numbers.length);
+            setCurrentIndex(index === 3 ? 2 : index);
           },
         },
       });
     }
   }, [numbers.length]);
 
+  React.useEffect(() => {
+    const images = document.querySelectorAll(".image-stack img");
+    gsap.to(images, {
+      yPercent: (index: number) => {
+        return index * 10;
+      },
+      scrollTrigger: {
+        trigger: sectionRef.current,
+        start: "top center",
+        end: "bottom top",
+        scrub: 1,
+        onUpdate: (self) => {
+          const progress = self.progress; // Получаем прогресс анимации
+          const offset = progress * 10; // Отступ меняется в зависимости от прогресса
+          gsap.to(images, {
+            y: (index) => index * 10 + offset, // Увеличиваем отступы между изображениями
+          });
+        },
+      },
+    });
+  }, []);
+
   return (
     <section
       ref={sectionRef}
-      className="flex items-center justify-between mb-[180px] max-xl:mb-[92px] max-xl:flex-col gap-10"
+      className="relative flex items-center justify-between mb-[180px] max-xl:mb-[92px] max-xl:flex-col max-xl:justify-center max-xl:h-screen gap-10"
     >
-      <div className="max-xl:w-full">
-        <Image
-          src="/figure-statistics.svg"
-          alt="polygon"
-          width={300}
-          height={300}
-        />
+      <div className="image-stack absolute max-xl:relative top-0 left-0 w-full min-h-[300px]">
+        {imageArray.map((src, index) => (
+          <Image
+            key={index}
+            className="absolute top-0 left-0 max-xl:w-[250px] max-xl:h-[120px]"
+            src={src}
+            alt="polygon"
+            width={320}
+            height={170}
+            style={{
+              top: `${index * 2}%`,
+              zIndex: index,
+            }}
+          />
+        ))}
       </div>
 
-      <div className="max-xl:w-full text-center">
+      <div className="max-xl:w-full text-left ml-auto">
         <h5
           ref={humberRef}
           className="font-etude w-[710px] max-xl:w-full max-xl:text-left font-medium text-[230px] max-xl:text-[180px] max-md:text-[80px] leading-[100%] text-primary"
